c749662db4a6ec77285f13276df0ab370a8fe337
Hikmet Meskic <64718591+hikmet-meskic@users.noreply.github.com>
2020-05-05 05:19:04 +0200

    Fixed object declarations

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './SearchBar.css';
 
-sortByOptions = {
+let sortByOptions = {
     'Best Match': 'best_match',
     'Highest Rated': 'rating',
     'Most Reviewed': 'review_count'
