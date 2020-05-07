955e93f75df5eaa2a954c86091976c5d4e9b7ac5
Hikmet Meskic <64718591+hikmet-meskic@users.noreply.github.com>
2020-05-07 23:07:16 +0200

    Implementation of Business component to query Yelp API

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,33 +1,28 @@
 const apiKey = 'fhY8a4-CHtrnCh8HWp4Kz1DPwEw1kk212le1MHXgXu8PuPoU2YSnQ3OSkgpSJ9niGfSdPQOp2CCQGMbZNcZLeg3TjrjsqbrJpo-XEFKsxZJ3uEnUwo8Bum3E-mu0XnYx';
 
-let Yelp = {
+export const Yelp = {
     search(term, location, sortBy) {
-        return fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
-        {
-            headers: {Authorization: `Bearer ${apiKey}`
-        }})
-        .fetch('https://cors-anywhere.herokuapp.com/')
-        .then(response => {
-            return response.json();
-        })
-        .then(jsonResponse => {
-            if(jsonResponse.businesses) {
-                return jsonResponse.businesses.map(business => {
-                    return {
-                        id: business.id,
-                        imageSrc: business.imageSrc,
-                        name: business.name,
-                        address: business.address,
-                        city: business.city,
-                        state: business.state,
-                        zipCode: business.zipCode,
-                        category: business.category,
-                        rating: business.rating,
-                        reviewCount: business.reviewCount
-                    }
-                })
-            }
-        })
-    }
-};
-export default Yelp;
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+          headers: {
+            Authorization: `Bearer ${apiKey}`
+          }
+        }).then(response => {
+          return response.json();
+        }).then(jsonResponse => {
+          if (jsonResponse.businesses) {
+            return jsonResponse.businesses.map(business => ({
+              id: business.id,
+              imageSrc: business.image_url,
+              name: business.name,
+              address: business.location.address1,
+              city: business.location.city,
+              state: business.location.state,
+              zipCode: business.location.zip_code,
+              category: business.categories[0].title,
+              rating: business.rating,
+              reviewCount: business.review_count
+            }));
+          }
+        });
+      }
+    };
